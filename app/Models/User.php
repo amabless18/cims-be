76a7b96d8f481b1e8a6195ff766d7ac74c4a1b27
Enrico Mabuka Jr <137061287+enriconutnull@users.noreply.github.com>
2024-03-25 11:11:05 +0800

    (chore) back end enhancement

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable
         'email',
         'password',
         'course',
+        'level',
         'branch',
         'coach_id',
         'date_reserved',
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('userType')->nullable();
+            $table->string('level')->nullable()->default('1');
             $table->string('branch')->nullable();
             $table->string('course')->nullable();
             $table->integer('phone')->nullable();
