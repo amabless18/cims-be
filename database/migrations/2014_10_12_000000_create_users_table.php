4b3d667727ff092df079ced564e2a192d9aca54e
Enrico <106855992+Jaykawaii20@users.noreply.github.com>
2024-04-05 18:25:28 +0800

    (chore) fix phone user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -27,7 +27,7 @@ return new class extends Migration
             $table->string('level')->nullable()->default('1');
             $table->string('branch')->nullable();
             $table->string('course')->nullable();
-            $table->integer('phone')->nullable();
+            $table->string('phone')->nullable();
             $table->string('status')->nullable()->default('pending');
             $table->unsignedBigInteger('coach_id')->nullable(); // Change data type to match primary key of 'users' table
             $table->foreign('coach_id')->references('id')->on('users')->onDelete('cascade'); // Add foreign key constraint$table->string('coach_id')->nullable();
